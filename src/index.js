eb62112e884c269ab9179ae7fa503076cb357e59
SeanPrendi <sean.prendi@gmail.com>
2020-04-15 22:37:19 -0400

    Random Data and Timezones   - Produces Random Data   - Data preference can be -1, 0, or 1   - Can specify Timezone (in America) by abbreviation   - Default timezone is EST TODO:   - Get random data when you query   - Reduce traffic when too full   - Number of people leaving a factor of ppl there

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const util = require("./util");
+const timezones = require("./timezones.json");
 
 const port = process.env.PORT || 5000;
 
@@ -15,9 +17,9 @@ const router = express.Router();
 /*
  * __________ API Routes __________
  */
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   let data = {
-    message: 'Hello from Coffee Shop Sim API',
+    message: "Hello from Coffee Shop Sim API"
   };
   res.status(200).send(data);
 });
@@ -28,12 +30,38 @@ router.get('/', (req, res) => {
     Number of customers out cannot be greater than 'curr'.
     Number of customer in will vary with time of day.
 */
-router.get('/customer-io', (req, res) => {
+router.get("/customer-io", (req, res) => {
+  var tooFull = false;
+  var UTCOffset;
+  var currHour = new Date().getHours();
+  if (req.query.max == null || req.query.curr == null) {
+    res.status(400).send({});
+    return;
+  }
+  if (req.query.timezone != null) {
+    let timezone = req.query.timezone;
+    console.log(req.query.timezone)
+    if (timezones[timezone] == null) {
+      res.status(400).send({}); // Unssuported timezone
+      return;
+    } else {
+      UTCOffset = timezones[timezone].UTCOffset;
+    }
+  } else {
+    UTCOffset = timezones.EST.UTCOffset; // Default use EST
+  }
+  let storeMax = req.query.max;
   let currentPop = req.query.curr;
+  if (currentPop >= Math.floor(0.9 * storeMax)) {
+    tooFull = true;
+  }
+
+  timeGroup = util.timeGroup(currHour);
+
   // Need to do some error checking here
   let data = {
     in: 5,
-    out: 3,
+    out: 3
   };
   res.status(200).send(data);
 });
@@ -43,7 +71,7 @@ router.get('/customer-io', (req, res) => {
  */
 
 // * Register routes
-app.use('/api', router);
+app.use("/api", router);
 
 // * Start server
 var server = app.listen(port, () =>
